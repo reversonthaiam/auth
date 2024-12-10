16db5fdd0dc181f2176bd7229db77b4f4ee49148
Reverson <reverson.santos@irancho.com.br>
2024-12-09 23:34:41 -0300

    pundit finished

--- db/migrate/202412080338327_create_articles.rb
+++ db/migrate/202412080338327_create_articles.rb
@@ -4,6 +4,7 @@ class CreateArticles < ActiveRecord::Migration[7.0]
       t.string :title
       t.text :description
 
+      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
